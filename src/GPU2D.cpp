0a46fb3a982165e83fc6e9ed52d1b287b19b9288
Alberto Fustinoni <claudiuslollarius@gmail.com>
2017-07-19 14:52:13 +0900

    build fixes

--- src/GPU2D.cpp
+++ src/GPU2D.cpp
@@ -1668,7 +1668,7 @@ void GPU2D::DrawBG_Text(u32 line, u32 bgnum)
         tilemapaddr += ((yoff & 0xF8) << 3);
 
     u16 curtile;
-    u16* curpal;
+    u16* curpal = nullptr;
     u32 pixelsaddr;
     u8 color;
 
